2138080d0c937aebaebc53276c3e6a79fa57b2c0
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2023-10-07 11:39:00 +0300

    linter fixed

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
+  rules: {
+    'space-before-function-paren': 0,
+    'vue/no-multiple-template-root': 0,
+    'vue/multi-word-component-names': 0,
+    'no-console': 'error'
+  }
+}
